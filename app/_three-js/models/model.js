a92b942d0aff79313557554190b8efe12995dabe
Romuru <pastelblue0721@gmail.com>
2023-07-06 15:37:02 +0900

    setup : README, image, styles, install etc....

--- app/_three-js/models/model.js
+++ app/_three-js/models/model.js
@@ -5,20 +5,24 @@ export default function Model(props) {
   const group = useRef();
   const { nodes, materials } = useGLTF("/Poimandres.gltf");
   return (
-    <group ref={group} {...props} dispose={null} scale={0.4}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Curve007_1.geometry}
-        material={materials["Material.001"]}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Curve007_2.geometry}
-        material={materials["Material.002"]}
-      />
-    </group>
+    <>
+      <ambientLight intensity={0} />
+      <directionalLight position={[1, 0.25, 1]} color={[0x00fffc, 0.3]} />
+      <group ref={group} {...props} dispose={null} scale={0.4}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Curve007_1.geometry}
+          material={materials["Material.001"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Curve007_2.geometry}
+          material={materials["Material.002"]}
+        />
+      </group>
+    </>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,5 @@ import Image from "next/image";
 import LootCanvas from "./_three-js/LootCanvas";
 
 export default function Home() {
-  return (
-    <div className="w-screen h-screen">
-      <LootCanvas />
-    </div>
-  );
+  return <div className="w-screen h-screen"></div>;
 }
