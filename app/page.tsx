eb36edbadcc9fc5e5183f691191a95b064f5c900
Romuru <pastelblue0721@gmail.com>
2023-07-07 18:35:22 +0900

    setup : new project

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import Image from "next/image";
-import LootCanvas from "./_three-js/LootCanvas";
-
-export default function Home() {
-  return <div className="w-screen h-screen"></div>;
-}
